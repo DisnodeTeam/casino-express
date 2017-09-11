7314058a89c90cb8f93ae5912b442c1667cace01
FireGamer3 <gm31334@gmail.com>
2017-09-10 20:05:44 -0500

    changed to auth function to be middleware

--- routes/api.js
+++ routes/api.js
@@ -4,34 +4,34 @@ var Database = require("../DB");
 var config = require("../config");
 Database.Connect();
 
+router.use((req, res, next) => {
+    if(req.headers.auth && req.headers.auth == config.auth){
+        next();
+    }else{
+        res.status(401).send({error: "You are unauthorized! please provide an `auth` header with a correct auth key!"});
+    }
+});
+
 router.get("/time", (req, res) => {
     res.send("some time left!");
 });
 router.get("/cobj",(req, res) => {
-    if(req.headers.auth && req.headers.auth == config.auth){
-        Database.Find("casinoObj", {"id":"cobj"}).then((cobj) => {
-            delete cobj[0]["_id"];
-            res.status(200).send(cobj[0]);
-        })
-    }else{
-        res.status(401).send({error: "You are unauthorized! plase provide an `auth` header with a correct auth key!"});
-    }
+    Database.Find("casinoObj", {"id":"cobj"}).then((cobj) => {
+        delete cobj[0]["_id"];
+        res.status(200).send(cobj[0]);
+    })
 });
 router.put("/cobj", (req,res) => {
-    if(req.headers.auth && req.headers.auth == config.auth){
-        if(req.body){
-            if(req.body.id && req.body.id == "cobj"){
-                Database.Update("casinoObj", {"id":"cobj"}, req.body.id).then(() => {
-                    res.status(200).send();
-                })
-            }else{
-                res.status(400).send({error: "Invalid Body"});
-            }
+    if(req.body){
+        if(req.body.id && req.body.id == "cobj"){
+            Database.Update("casinoObj", {"id":"cobj"}, req.body.id).then(() => {
+                res.status(200).send();
+            })
         }else{
-            res.status(400).send({error: "No Body"});
+            res.status(400).send({error: "Invalid Body"});
         }
     }else{
-        res.status(401).send({error: "You are unauthorized! plase provide an `auth` header with a correct auth key!"});
+        res.status(400).send({error: "No Body"});
     }
 })
 
