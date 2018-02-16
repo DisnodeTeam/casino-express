1c2213f96c76f0c7aba3af7afbbfc70b8a7d1eba
Unknown <livewire192@gmail.com>
2018-02-15 20:25:14 -0800

    forgot check

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,21 +5,23 @@ var Database = require("../DB");
 Database.Connect().then(() => {});
 
 router.post("/", (req, res) => {
-  Database.Find("players", {
-    id: req.body.user
-  }).then((player) => {
-    if (player[0] != undefined) {
-      if (player[0].voteCount == undefined) {
-        player[0].voteCount = 1;
-      } else {
-        player[0].voteCount++;
+  if (req.body.type == 'upvote') {
+    Database.Find("players", {
+      id: req.body.user
+    }).then((player) => {
+      if (player[0] != undefined) {
+        if (player[0].voteCount == undefined) {
+          player[0].voteCount = 1;
+        } else {
+          player[0].voteCount++;
+        }
+        player[0].money += (100000 * player[0].voteCount);
+        Database.Update('players', {
+          id: player[0].id
+        }, player[0]);
       }
-      player[0].money += (100000 * player[0].voteCount);
-      Database.Update('players', {
-        id: player[0].id
-      }, player[0]);
-    }
-    res.status(200);
-  })
+    })
+  }
+  res.status(200);
 });
 module.exports = router;
