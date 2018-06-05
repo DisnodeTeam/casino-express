4bc2c5e7943fa8b1134d8d0fd2a3e7467bad3eed
Unknown <livewire192@gmail.com>
2018-06-05 16:30:06 -0700

    redid voting and added auth

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,28 +5,44 @@ var Database = require("../DB");
 Database.Connect().then(() => {});
 
 
-router.post("/", (req, res) => {
-  if (req.body.type == 'upvote') {
-    Database.Find("players", {
-      id: req.body.user
-    }).then((player) => {
-      if (player[0] != undefined) {
-        if (true) {
-          if (player[0].voteCount == undefined) {
-            player[0].voteCount = 1;
-          } else {
-            player[0].voteCount++;
-          }
-          player[0].money += (100000 * player[0].voteCount);
-          player[0].lastVote = new Date().getTime()
-          logger.Info('WebhookRouter', 'Voted', `${player[0].name} ${player[0].id} received $${(100000 * player[0].voteCount)}`)
-          Database.Update('players', {
-            id: player[0].id
-          }, player[0]);
-        } else logger.Info('WebhookRouter', 'Voted Already', `${player[0].name} ${player[0].id}`)
-      }
-    })
+
+router.use((req, res, next) => {
+  if (req.headers.authorization === config.webhook) {
+    logger.Success("Webhook Route", "Auth", "IP: " + req.ip + " -> " + req.method + " " + req.url);
+    next();
+  } else {
+    logger.Warning("Webhook Route", "DENY FROM AUTH", "IP: " + req.ip + " -> " + req.method + " " + req.url);
+    res.status(401).send({
+      error: "You are unauthorized! please provide an `auth` header with a correct auth key!"
+    });
   }
-  res.status(200);
 });
+
+router.post("/", async (req, res) => {
+  if (req.body.type === 'upvote') {
+    const [player] = await Database.Find('players', {
+      id: req.user.id
+    });
+    if (player) {
+      if (player.voteCount) {
+        player.voteCount++;
+      } else {
+        player.voteCount = 1;
+      }
+      player.money += (100000 * player.voteCount);
+      player.lastVote = new Date().getTime();
+      logger.Info('Webhook Route', 'Vote', `${player.name} ${player.id} received $${(100000 * player.voteCount)}`)
+      await Database.Update('players', {
+        id: player.id
+      }, player);
+    }
+    res.status(200).end();
+  } else if (req.body.type === 'test') {
+    logger.Info('Webhook Route', 'Test', 'All good');
+    res.status(200).end();
+  } else {
+    res.status(400).send('Bad Request');
+  }
+});
+
 module.exports = router;
